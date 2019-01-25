018b2cde5fd6dc4ddb7b3ae83333c0f4745c0696
Connor Avant <connor.avant@gmail.com>
2019-01-24 19:59:28 -0600

    Testing to look at syntax for Seeing what a player has in their inventory

---
+++ StardewImportantInventory/StardewImportantInventory/ItemCatcher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StardewModdingAPI;
+using StardewValley;
+
+namespace StardewImportantInventory
+{
+    internal class Modentry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Char CatchItems;
+
+        private void getItems(object sender, EventArgs e)
+        {
+            if (Game1.player.hasItemInInventoryNamed("Rainbow Scale"))
+            this.Monitor.Log($"Player has a Rainbow Scale");
+
+
+        }
+
+
+    }
+}
+
+
+
+
